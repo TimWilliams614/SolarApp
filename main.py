f85b42087ecc8767de891a7d1de8a6294447f15f
Tim Williams <timwilliams614@gmail.com>
2016-03-11 14:39:33 -0800

    Consumption Screen

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from kivy.config import Config
 
 #---Initial Configuration---#
 Config.set('graphics','borderless','1') #removes border decoration
-Config.set('graphics','height','400') #static height due to tablet constraints
+Config.set('graphics','height','480') #static height due to tablet constraints
 Config.set('graphics','width','800') #static width due to tablet constraints
-Config.set('graphics','resizable','0') #Make unresizable so user can't resize
+Config.set('graphics','resizable','0') #Make unresizable so user can't resize or move whole window
 #---End of Configuration---#
 
 
@@ -41,13 +41,13 @@ class AboutScreen(Screen):
 class SolarApp(App):
     
     def build(self):
-        self.manager = ScreenManager() #Kivy Screen Manager object to handle 
+        self.manager = ScreenManager() #Kivy Screen Manager object to handle screen transistions
     	self.manager.add_widget(IdleScreen(name='idle'))
     	self.manager.add_widget(LockerScreen(name='locker'))
     	self.manager.add_widget(ConsumptionScreen(name='consumption'))
     	self.manager.add_widget(AboutScreen(name='about'))
         
-        layout = FloatLayout(size=(800,400))
+        layout = FloatLayout(size=(800,480))
         layout.add_widget(self.manager)
         layout.add_widget(NavBar(id='my_root',name='root'))
 
